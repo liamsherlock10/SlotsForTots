86c86d81ea868e7d4d855626814bada72213637a
liamsherlock10 <109117361+liamsherlock10@users.noreply.github.com>
2024-11-08 12:22:23 -0500

    added a basename for the router in app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename= "/SlotsForTots">
       <Routes>
         <Route path= "/" element={<Home />} >/</Route>
       </Routes>
